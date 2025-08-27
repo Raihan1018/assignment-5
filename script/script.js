3ca496a606c890c450d6ab8f1eb42a88ccc71fee
Raihan <raihanuddinahmed10@gmail.com>
2025-08-27 14:22:06 +0600

    heart count functionality added

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
-console.log("ok");
+const heartButtons = document.querySelectorAll("#heart-btn");
+
+// Select all heart number spans (both mobile and desktop)
+const heartNumbers = document.querySelectorAll("#heart-number");
+
+heartButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    heartNumbers.forEach((span) => {
+      let current = parseInt(span.textContent);
+      span.textContent = current + 1;
+    });
+  });
+});
